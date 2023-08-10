6a60c77461e49a25fd00887675c6795184362810
jirawat malaiwan <tawan.jirawat@gmail.com>
2023-08-10 22:34:04 +0700

    9

--- Homework/HW04/hw04-2-operator.cpp
+++ Homework/HW04/hw04-2-operator.cpp
@@ -25,6 +25,6 @@ int main() {
     printf( "a %%= 1 %d \n",a %= 1 ) ;
     printf( "a %%= b %d \n",a %= b ) ;
     printf( "a && b %d \n",a && b ) ;
-    printf( "a || b %d \n",a || b ) ;
+    printf( "a || b %d \n",a || b) ;
     return 0 ;
 }
