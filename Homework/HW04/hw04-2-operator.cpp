b8052039a2dd2873f4b65f07633767f69f518aa7
jirawat malaiwan <tawan.jirawat@gmail.com>
2023-08-10 18:17:34 +0700

    1

--- Homework/HW04/hw04-2-operator.cpp
+++ Homework/HW04/hw04-2-operator.cpp
@@ -5,7 +5,7 @@
 int main() {
     int a = 0, b = 0 ;
     printf( "Please enter value (a b) : " ) ;
-    scanf( "%d %d", a, b ) ; //TODO: #11 Input variable 'a' and 'b'
+    scanf( "%d %d", &a, &b ) ; //TODO: #11 Input variable 'a' and 'b'
 
     //TODO: #12 Complete operator, and display all operator output. (and fix all bugs.)
     printf( "a = %d , b = %d \n", a, b ) ;
@@ -13,7 +13,7 @@ int main() {
     printf( "a - b = %d \n",  a-b ) ;
     printf( "a * b = %d \n", a*b ) ;
     printf( "a / b = %d (b must greater than 0)\n", b > 0 ? a : 0 ) ;
-    printf( "a % b = %d \n", a%b ) ;
+    printf( "a %% b = %d \n", a%b ) ;
     printf( "++a = %d \n",  ++a ) ;
     printf( "a-- = %d \n", a-- ) ;
     printf( "a += 1 %d \n", a+=1 ) ;
@@ -22,8 +22,8 @@ int main() {
     printf( "a -= b %d \n", a-=b ) ;
     printf( "a *= 1 %d \n", a*=1 ) ;
     printf( "a *= b %d \n", a*=b ) ;
-    printf( "a %= 1 %d \n", a%=1 ) ;
-    printf( "a %= b %d \n", a%=1 ) ;
+    printf( "a %%= 1 %d \n", a%=1 ) ;
+    printf( "a %%= b %d \n", a%=1 ) ;
     printf( "a && b %d \n", a&&b ) ;
     printf( "a || b %d \n", a || b ) ;
     return 0 ;
