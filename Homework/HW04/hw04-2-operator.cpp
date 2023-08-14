d030856f134e53596d1920936fe5a39af58c7ad4
jirawat malaiwan <tawan.jirawat@gmail.com>
2023-08-14 13:16:48 +0700

    55

--- Homework/HW04/hw04-2-operator.cpp
+++ Homework/HW04/hw04-2-operator.cpp
@@ -3,7 +3,7 @@
 */
 #include <stdio.h>
 int main() {
-    int a = 0, b = 0 ;
+    int a = 2, b = 1 ;
     printf( "Please enter value (a b) : " ) ;
     scanf( "%d %d", &a, &b ) ; //TODO: #11 Input variable 'a' and 'b'
 
@@ -15,7 +15,7 @@ int main() {
     printf("a / b = %d (b must greater than 0)\n", b > 0 ? a : 0 ) ;
     printf("a %% b = %d \n",a%b ) ;
     printf("++a = %d \n",++a ) ;
-    printf("a-- = %d \n",a-- ) ;
+    printf("a-- = %d \n",a -- ) ;
     printf("a += 1 %d \n",a += 1 ) ;
     printf("a += b %d \n",a += b ) ;
     printf("a -= 1 %d \n",a -= 1 ) ;
